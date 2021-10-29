9921e9e415375234777df946ed96f7b6a16248ec
EhoodGoren <ehoodgoren@gmail.com>
2021-10-29 22:18:55 +0300

    Clicking on a cell now updates the model's array at the index of the clicked cell

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 class Model {
     constructor(){
-        
+        this.boardCells = Array(49).fill();
     }
+
+    play(index) {
+        this.boardCells[index] = 'X';
+    }
+
 }
 
 class View {
     constructor(){
+    }
 
+    render() {
+        const board = document.createElement('div');
+        board.setAttribute('id','board');
+
+        const boardCells = Array(49).fill().map((_, index) => {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            // cell.addEventListener('click', () => {})
+
+            board.appendChild(cell);
+        });
+        
+        document.body.appendChild(board);
+    }
+
+    cellClicked(handler) {
+        const cells = [...document.querySelectorAll('.cell')];
+        cells.map((cell, index) => {
+                cell.addEventListener('click', () => {
+                    handler(index);
+                })
+        });
     }
 }
 
 class Controller {
     constructor(){
-
+        this.model = new Model();
+        this.view = new View();
+    }
+    
+    start() {
+        this.view.render();
+        this.view.cellClicked((index)=>{this.model.play(index)});
     }
 }
+
+const connectFour = new Controller();
+connectFour.start();
