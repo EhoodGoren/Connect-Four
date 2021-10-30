0e962714289c92b2cf1c2cda674cc895f64a7713
EhoodGoren <ehoodgoren@gmail.com>
2021-10-30 22:33:03 +0300

    Winner message is now logged if a player connected 4 discs (all axes calculated)

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 class Model {
     constructor(){
         this.boardCells = Array(49).fill();
-        // this.boardCells = Array(7).fill(Array(7).fill());
         this.currentPlayer = '🔴';
     }
 
@@ -14,22 +13,6 @@ class Model {
             cellToDropIndex = cellToDropIndex - 7;
         }
         this.boardCells[cellToDropIndex] = this.currentPlayer;
-        // const _2DIndex = this.indexTo2D(index);
-        // If the click was on a taken cell, it is ignored.
-        // if((this.boardCells[_2DIndex.column])[_2DIndex.row] !== undefined) return 'taken';
-
-        // const cellColumn = this.boardCells[_2DIndex.column];
-        // let cellCounter = 1;
-        // let cellToDrop = cellColumn[cellColumn.length - cellCounter];
-        // while(cellToDrop !== undefined){
-            // cellCounter++;
-            // if(cellCounter > 7) return 'taken' // (Whole column)
-            // cellToDrop = cellColumn[cellColumn.length - cellCounter];
-        // }
-        // this.boardCells[index] = this.currentPlayer;
-        // this.boardCells[_2DIndex.column][_2DIndex.row] = this.currentPlayer;
-        // console.log(this.boardCells);
-        // const cellDropIndex = this.indexTo1D({column: _2DIndex.column, row: cellCounter - 1});
         // Check tie
         if(this.checkDraw()) console.log("It's a draw");
         // Check win
@@ -40,68 +23,76 @@ class Model {
         return {cell: cellToDropIndex, player: playerBeforeSwitch};
     }
 
-    // indexTo2D(index){
-    //     return {
-    //         column: index%7,
-    //         row: Math.floor(index/7)
-    //     }
-    // }
-    // indexTo1D(index){
-    //     return index.row + index.column;
-    // }
-
     switchPlayer() {
         this.currentPlayer = this.currentPlayer === '🔴' ? '🟡' : '🔴';
     }
 
-    /* * */checkDraw() {
-        // for(let column of this.boardCells){
-        //     for(let cell of column){
-        //         if(cell !== undefined) return false
-        //     }
-        // }
-        // return true;
+    checkDraw() {
         return this.boardCells.every(cell => cell !== undefined);
     }
 
     checkWin(player) {
-    //     if(player === '🟡') return
-    //     const playerCells = this.getPlayerCells(player);
-    //     // const playerCells = this.boardCells.filter((cell,index) => cell === player);
-    //     // playerCells now holds all index of the player in the board
-    //     for(let cellIndex of playerCells){
-    //         const testing = this.checkStreak('+1', cellIndex, player)
-    //         if(testing) break;
-    //     }
-    //     /*playerCells.forEach(cellIndex => {
-    //         // this.checkStreak(up)
-    //         // this.checkStreak(up_right)
-    //         // this.checkStreak(right, cellIndex, player)
-    //         const testing = this.checkStreak('+1', cellIndex, player)
-    //         console.log(testing)
-    //         // this.checkStreak(down_right)
-    //         // this.checkStreak(down)
-    //         // this.checkStreak(down_left)
-    //         // this.checkStreak(left)
-    //         // this.checkStreak(up_left)
-    //     })*/
+        const playerCells = this.getPlayerCells(player);
+        // const playerCells = this.boardCells.filter((cell,index) => cell === player);
+        // playerCells now holds all index of the player's discs in the board
+        for(let cellIndex of playerCells){
+            const testRight = this.checkSequence('+1', cellIndex, player) // Right
+            if(testRight){
+                console.log('WINNER!!!!!!!!!')
+                break;
+            }
+            const testDown = this.checkSequence('+7', cellIndex, player) // Down
+            if(testDown){
+                console.log('WINNER!!!!!!!!!')
+                break;
+            }
+            const testRightDown = this.checkSequence('+8', cellIndex, player) // Right Down
+            if(testRightDown){
+                console.log('WINNER!!!!!!!!!')
+                break;
+            }
+            const testRightUp = this.checkSequence('-6', cellIndex, player) // Right Up
+            if(testRightUp){
+                console.log('WINNER!!!!!!!!!')
+                break;
+            }
+        }
     }
 
-    // checkSequence(direction, cellIndex, player, sequence = 1) {
-    //     if(sequence === 4) {
-    //         return true;
-    //     }
-    //     // const checkCellIndex = `${cellIndex}${direction}`;
-    //     const checkCellIndex = cellIndex + `${direction}`;
-    //     const checkCell = this.boardCells[checkCellIndex];
-    //     if(checkCell !== player){
-    //             return false;
-    //         }
-    //     sequence++;
-    //     return this.checkStreak(direction, checkCellIndex, player, sequence);
-    // }
+    checkSequence(direction, cellIndex, player, sequence = 1) {
+        if(sequence === 4) return true;
+        if(this.checkEdge(direction, cellIndex)) return false;
+        const checkCellIndex = cellIndex + Number(direction);
+        const checkCell = this.boardCells[checkCellIndex];
+        if(checkCell !== player){
+                return false;
+        }
+        sequence++;
+        return this.checkSequence(direction, checkCellIndex, player, sequence);
+    }
+
+    checkEdge(direction, cellIndex) {
+        switch (direction) {
+            case '+1':
+                return (cellIndex - 6) % 7 === 0 ? true : false;
+                break;
+            case '+7':
+                return Math.floor(cellIndex / 7) === 6 ? true : false;
+                break;
+            case '+8':
+                return (cellIndex - 6) % 7 === 0 || Math.floor(cellIndex / 7) === 6 ? true : false;
+                break;
+            case '-6':
+                return (cellIndex - 6) % 7 === 0 || cellIndex < 7 ? true : false;
+                break
+            default:
+                return false
+                break;
+        }
+    }
 
     getPlayerCells(player) {
+        // Replace whole function with filter?
         const playerCells = [];
         this.boardCells.forEach((cell, index) => {
             if(cell === player){
@@ -118,7 +109,6 @@ class View {
     }
 
     render() {
-        // const board = document.createElement('div');
         // Board setup
         this.board = document.createElement('div');
         this.board.setAttribute('id','board');
@@ -141,10 +131,7 @@ class View {
         const cells = [...document.querySelectorAll('.cell')];
         cells.map((cell, index) => {
                 cell.addEventListener('click', () => {
-                    // const discData = handler(index); // Updates model array and returns current player
-                    // if(discData === 'taken') return;
-                    // cells[discData.cell].innerText = discData.player;
-                    const discData = handler(index); // Updates model array and returns current player
+                    const discData = handler(index);
                     if(discData === 'taken') return;
                     cells[discData.cell].innerText = discData.player;
                 })
