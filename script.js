104c3160d4c935b6df037a63f2bec96fd658231d
EhoodGoren <ehoodgoren@gmail.com>
2021-10-30 13:48:40 +0300

    Displays the player's play in each clicked cell (X or O). Players are switched after every turn.

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 class Model {
     constructor(){
         this.boardCells = Array(49).fill();
+        this.currentPlayer = 'X';
     }
 
     play(index) {
-        this.boardCells[index] = 'X';
+        this.boardCells[index] = this.currentPlayer;
+        const playerBeforeSwitch = this.currentPlayer;
+        this.switchPlayer();
+        return playerBeforeSwitch;
     }
 
+    switchPlayer() {
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+    }
 }
 
 class View {
     constructor(){
+        this.board;
     }
 
     render() {
-        const board = document.createElement('div');
-        board.setAttribute('id','board');
+        // const board = document.createElement('div');
+        this.board = document.createElement('div');
+        this.board.setAttribute('id','board');
 
         const boardCells = Array(49).fill().map((_, index) => {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             // cell.addEventListener('click', () => {})
 
-            board.appendChild(cell);
+            this.board.appendChild(cell);
         });
         
-        document.body.appendChild(board);
+        document.body.appendChild(this.board);
     }
 
     cellClicked(handler) {
         const cells = [...document.querySelectorAll('.cell')];
         cells.map((cell, index) => {
                 cell.addEventListener('click', () => {
-                    handler(index);
+                    const player = handler(index); // Updates model array and returns current player
+                    cells[index].innerText = player;
                 })
         });
     }
@@ -46,7 +56,7 @@ class Controller {
     
     start() {
         this.view.render();
-        this.view.cellClicked((index)=>{this.model.play(index)});
+        this.view.cellClicked((index)=>{ return this.model.play(index)});
     }
 }
 
